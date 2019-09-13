c1bea45f0253ed1b879eaf459e9db7e2c965b68a
unknown <kuba.farionn@gmail.com>
2019-09-13 09:30:10 +0200

    Compile This

---
+++ B1L1CompileThis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B1L1CompileThis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo mensen!");
+        }
+    }
+}
